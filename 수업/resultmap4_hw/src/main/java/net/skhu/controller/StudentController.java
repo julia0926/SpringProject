f696ba6f7f80b93fa6fe5f1852edd72835c1d243
kimjiin <kj980926@naver.com>
2021-10-24 00:01:07 +0900

    연습문제 - resultMap4 구현 방식으로

---
+++ 수업/resultmap4_hw/src/main/java/net/skhu/controller/StudentController.java
@@ -0,0 +1,25 @@
+package net.skhu.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.skhu.dto.Department;
+import net.skhu.mapper.DepartmentMapper;
+
+@Controller
+public class StudentController {
+
+    @Autowired DepartmentMapper departmentMapper;
+
+    @RequestMapping("department/list")
+    public String list(Model model) {
+        model.addAttribute("departments", departmentMapper.findAll());
+        return "department/list";
+    }
+
+}
+
