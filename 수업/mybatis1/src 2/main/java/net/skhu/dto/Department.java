8254e1a78bc1c2797c97a1ff2e5075c61cf8d614
kimjiin <kj980926@naver.com>
2021-11-12 15:49:41 +0900

    mybatis 기초

---
+++ 수업/mybatis1/src 2/main/java/net/skhu/dto/Department.java
@@ -0,0 +1,27 @@
+package net.skhu.dto;
+
+import java.io.Serializable;
+
+public class Department implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+
+    int id;
+    String departmentName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+}
